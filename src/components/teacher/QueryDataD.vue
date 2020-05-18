6e732188ed421dfdfe6d234557f62bf5892ca054
Ran Miller <ranmiller.leaf@gmail.com>
2020-05-19 00:10:48 +0800

    zr complete get data and beautify style

--- src/components/teacher/QueryDataD.vue
+++ src/components/teacher/QueryDataD.vue
@@ -58,29 +58,31 @@
                         </el-table-column>
                         <el-table-column
                             prop="name"
+                            align="center"
                             label="姓名"
                             width="180">
                         </el-table-column>
                         <el-table-column
                             prop="state"
                             label="后勤状态"
-                            width="140"
-                            :filters="[{ text: '不满足条件', value: '0' }, { text: '满足条件', value: '1' }]"
+                            align="center"
+                            :filters="[{ text: '未完成', value: '0' }, { text: '已完成', value: '1' }]"
                             :filter-method="filterTag"
                             filter-placement="bottom-end">
                             <template slot-scope="scope">
                                 <el-tag
                                 :type="scope.row.state=='1' ? 'success' : 'warning'"
-                                disable-transitions>{{scope.row.state}}</el-tag>
+                                disable-transitions>{{scope.row.state == '0'? '未完成':'已完成'}}</el-tag>
                             </template>
                         </el-table-column>
                         <el-table-column
                             prop="remark"
                             label="备注"
+                            align="center"
                             width="250">
                         </el-table-column>
                         <el-table-column
-                            label="操作">
+                            label="操作" align="center">
                             <template slot-scope="scope">
                                 <el-button
                                 size="mini"
@@ -140,6 +142,7 @@
             }
         },
         methods: {
+            // 查询的方法
             onQuery() {
                 console.log("提交查询")
             },
@@ -178,6 +181,7 @@
                 }).then(res => {
                     console.log("获取后勤数据成功!");
                     console.log(res);
+                    this.tableData = res.data;
                 }).catch(error => {
                     console.log("公告获取失败");
                     console.log(error.response.data);
@@ -200,6 +204,10 @@
 </script>
 
 <style scoped>
+/* 
+ *带有scroped的style标签
+ *是有作用域的，其包含的样式只作用于当前组件
+ */
 .locat {
     position: absolute;
     top: 20px;
@@ -215,14 +223,15 @@
     margin-left: 40px;
 }
 .data {
-    position: absolute;
+    /* position: absolute; */
     /* border: 1px solid #000; */
-    width: 100%;
+    width: 80%;
+    height: 100%;
+    margin: 0 auto;
 }
 .delete {
     /* border: 1px solid #000; */
-    float: left;
-    margin-left: 40px;
+    float: left; 
     margin-top: 10px;
 }
 .query {
@@ -235,7 +244,7 @@
 .manage{
     /* border: 1px solid red; */
     clear: both;
-    margin-left: 40px;
+    /* margin-left: 40px; */
 }
 .page {
     /* border: 1px solid #000; */
