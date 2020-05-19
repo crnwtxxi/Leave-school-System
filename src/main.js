934bc7e1ea1bbc92872f509a5d1061fce79756d2
Ran Miller <ranmiller.leaf@gmail.com>
2020-05-19 18:20:26 +0800

    zr import filterXSS

--- src/main.js
+++ src/main.js
@@ -10,6 +10,28 @@ import mavonEditor from 'mavon-editor'
 import 'mavon-editor/dist/css/index.css'
 Vue.use(mavonEditor)
 
+import filterXSS from 'xss'
+filterXSS.whiteList['input'] = ['type', 'src', 'disabled', 'checked', 'class']
+filterXSS.whiteList['img'] = ['src', 'alt', 'title', 'width', 'max-width', 'style', 'height']
+filterXSS.whiteList['math'] = []
+filterXSS.whiteList['semantics'] = []
+filterXSS.whiteList['mrow'] = []
+filterXSS.whiteList['mtext'] = []
+filterXSS.whiteList['annotation'] = ['encoding']
+filterXSS.whiteList['msub'] = []
+filterXSS.whiteList['mi'] = []
+filterXSS.whiteList['mo'] = []
+filterXSS.whiteList['mn'] = []
+filterXSS.whiteList['li'] = ['class']
+filterXSS.whiteList['span'] = ['class', 'title', 'aria-hidden', 'style']
+filterXSS.whiteList['code'] = []
+filterXSS.whiteList['li'] = ['class', 'id']
+filterXSS.whiteList['td'] = ['style', 'class']
+filterXSS.whiteList['p'] = ['style', 'class']
+filterXSS.whiteList['div'] = ['style', 'class']
+filterXSS.whiteList['a'] = ['style', 'class', 'id', 'target', 'href', 'title']
+filterXSS.stripIgnoreTag = true
+Vue.prototype.filterXSS = filterXSS;
 
 Vue.config.productionTip = false
 
