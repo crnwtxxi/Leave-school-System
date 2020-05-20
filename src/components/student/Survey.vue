d9f27f8a7322ae507e84f20d0a5d1a7ee71f5e95
IAMjmj <17360075977@163.com>
2020-05-20 15:57:28 +0800

    (survey,libary):修改了页面交互，以及一些优化

--- src/components/student/Survey.vue
+++ src/components/student/Survey.vue
@@ -13,11 +13,11 @@
         <div v-for="item in questionData.choice">
           <template>
             <h4>{{item.title}}</h4>
-            <el-radio-group v-model="item.radio" >
-            <el-radio label="1">{{item.radio}}</el-radio>
-            <el-radio label="2">{{item.B.title}}</el-radio>
-            <el-radio label="3">{{item.C.title}}</el-radio>
-            <el-radio  label="4">{{item.D.title}}</el-radio>
+            <el-radio-group v-model="item.radio">
+              <el-radio label="1">{{item.A.title}}</el-radio>
+              <el-radio label="2">{{item.B.title}}</el-radio>
+              <el-radio label="3">{{item.C.title}}</el-radio>
+              <el-radio label="4">{{item.D.title}}</el-radio>
             </el-radio-group>
           </template>
         </div>
@@ -30,284 +30,205 @@
       </div>
       <div id="submit">
         <el-button type="success" round v-on:click="verify">提交</el-button>
+        <el-button type="success" round v-on:click="handleBack">返回</el-button>
       </div>
-      
+
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      msg: "请求成功",
-      announceCount: 18,
-      announceViewCount: 18,
-      // 表格的数据
-      tableViewData: [],
-      //题目数据
-      title:"x",
-      username:"",
-      questionData:{
-        choice:[
-                {
-                  title:"你对本次的服务满意程度是多少?",
-                  option:[
-                      {title:"满意"},
-                      {title:"还行"},
-                      {title:"一般"},
-                      {title:"不满意"}
-                 
-                  ],
-                   radio:"1"
-                }
-        ],
-        questionAnswer:[
-              {title:"你还要其他建议吗?",anstion:""}
-        ]
+  import surveylist from '@/components/student/SurveyList'
+  export default {
+    data() {
+      return {
+        msg: "请求成功",
+        announceCount: 18,
+        sno: "",
+        announceViewCount: 18,
+        // 展示表格的数据
+        tableViewData: [],
+        //题目数据
+        title: "x",
+        questionData: {
+          choice: [{
+            title: "",
+            A:{title:""},
+            B:{title:""},
+            C:{title:""},
+            D:{title:""},
+            radio: "1"
+          }],
+          questionAnswer: [{
+            title: "你还要其他建议吗?",
+            anstion: ""
+          }]
+        },
+        search: "",
+        currentPage: 1,
+        pageSize: 1
+      };
+    },
+    methods: {
+      //更新简答题，并做xss预防
+      update(event, val) {
+        this.questionData.questionAnswer[val].anstion = this.stripscript(event.currentTarget.value)
       },
-      search: "",
-      currentPage: 1,
-      pageSize: 1
-    };
-  },
-  methods: {
-    //   查看公告的点击事件
-    update(event,val){
-       console.log(val)
-       console.log(event.currentTarget.value)
-       this.questionData.questionAnswer[val].anstion=event.currentTarget.value  
+      handleBack(){
+        this.$router.go(-1)
+      },
+       //进行xss过滤
+       stripscript(s)
+       {
+       //过滤掉敏感的字母,预防xss攻击
+       var pattern = new RegExp("[%--`~!@#$^&*()=|{}''\\[\\]<>/?~！@#￥……&*（）――|{}【】‘”“']")
+         var rs = "";
+         for (var i = 0; i < s.length; i++) { rs=rs+s.substr(i,1).replace(pattern,''); } 
+       return rs; 
+       },
 
-    }
-    ,
-    handleView(index, row) {
-      this.$router.push({ path: "/teacher/configsurvey", query: { id: row.id } });
-    },
-    // 设置公告的有效性
-    setTag(index, row) {
-      if (this.tableViewData[index].tag == "有效") {
-        this.tableViewData.some((item, i) => {
-          if (item.id == row.id) {
-            this.tableViewData[i].tag = "无效";
-          }
-        });
-        this.tableData.some((item, i) => {
-          if (item.id == row.id) {
-            this.tableData[i].tag = "无效";
-          }
-        });
-      } else {
-        this.tableViewData.some((item, i) => {
-          if (item.id == row.id) {
-            this.tableViewData[i].tag = "有效";
-          }
-        });
-        this.tableData.some((item, i) => {
-          if (item.id == row.id) {
-            this.tableData[i].tag = "有效";
-          }
-        });
-      }
-    },
-    // 删除公告的点击事件
-    handleDelete(index, row) {
-      this.tableData.some((item, index) => {
-        if (item.id == row.id) {
-          this.tableData.splice(index, 1);
-          console.log("删除标题为----《" + item.title + "》----的调查！");
-          this.announceCount = this.announceCount - 1;
-          if (
-            this.announceCount % this.pageSize == 0 &&
-            this.announceCount != 0 &&
-            this.currentPage != 1
-          ) {
-            this.handleCurrentChange(this.currentPage - 1);
-          } else {
-            this.handleCurrentChange(this.currentPage);
-          }
-          return true;
-        }
-      });
-    },
-    // pageSize 改变时会触发
-    handleSizeChange(val) {
-      console.log(`每页大小为：${val}`);
-      this.pageSize = val;
-      this.handleCurrentChange(1);
-    },
-    // currentPage 改变时会触发
-    handleCurrentChange(val) {
-      console.log(`当前 ${val} 页`);
-      this.currentPage = val;
-      this.tableViewData.splice(0, this.tableViewData.length);
-      for (
-        var i = (this.currentPage - 1) * this.pageSize;
-        i < this.currentPage * this.pageSize && i < this.announceCount;
-        i++
-      ) {
-        this.tableViewData.push(this.tableData[i]);
-      }
-    },
-
-    // 筛选标记位
-    filterTag(value, row) {
-      return row.tag === value;
-    },
-    verify() {
-                
-
-                
-
-                this.$confirm('确认提交？，是否继续?', '提示', {
-                    confirmButtonText: '确定',
-                    cancelButtonText: '取消',
-                    type: 'warning'
-                }).then(() => {
-                  this.$axios({
-                            method: 'post',
-                            url: 'http://106.15.206.229/survey/finish',
-                            withCredentials : true,
-                            data: {
-                              username:this.username,
-                               id:this.$route.query.id,
-                               title:this.title,
-                               choice:this.questionData.choice,
-                               questionAnswer:this.questionData.questionAnswer
-                            },
-                            header: {
-                                'Content-Type': 'application/json;charset=UTF-8'
-                            }
-                        }).then((res) => {
-                            console.log('111');
-                            console.log(res);
-                            this.$notify({
-                                title: "发布成功",
-                                offset: 100,
-                                type: "success",
-                                showClose: false,
-                                duration: 1500
-                            });
-                            this.$emit('func');
-                        }).catch((error) => {
-                             this.$message({
-                                     type: 'info',
-                                     message: '发布失败，请重新尝试！'
-                                 });  
-                            
-                            console.log('失败');
-                            console.log(error.response.data);
-                            console.log(error.response.status);
-                            console.log(error.response.headers); 
-                            console.log('Error', error.message);
-                            console.log(error.config);
-                        })
-                this.$message({
-                    type: 'success',
-                    message: '调查提交成功！'
-                });
-                }).catch(() => {
-                    this.$message({
-                        type: 'info',
-                        message: '取消提交！'
-                    });          
-                });
+      // 筛选标记位
+      filterTag(value, row) {
+        return row.tag === value;
+      },
+      verify() {
+        this.$confirm('确认提交？是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$axios({
+            method: 'post',
+            url: '/api/survey/finish',
+            withCredentials: true,
+            data: {
+              id: this.$route.query.id,
+              sno: this.sno,
+              choice: this.questionData.choice,
+              questionAnswer: this.questionData.questionAnswer
+            },
+            header: {
+              'Content-Type': 'application/json;charset=UTF-8'
             }
-  },
-  mounted() {
-       var user=JSON.parse(sessionStorage.getItem('user'));
-       console.log(user.username)
-       console.log('hahhah')
-      var Sid=this.$route.query.id
-      //这里记得最后 要改
+          }).then((res) => {
+            this.$notify({
+              title: "发布成功",
+              offset: 100,
+              type: "success",
+              showClose: false,
+              duration: 1500
+            });
+            this.$emit('func');
+          }).catch((error) => {
+            this.$notify({
+            title: "填写失败",
+            offset: 100,
+            type: "error",
+            showClose: false,
+            duration: 1500
+            });
+          })
+          
+        }).catch(() => {
+          this.$notify({
+          title: "取消",
+          offset: 100,
+          type: "error",
+          showClose: false,
+          duration: 1500
+          });
+        });
+      }
+    },
+    created(){
+        //访问后台接口，获取数据
+         var user = JSON.parse(sessionStorage.getItem('user'))
+      
+         this.sno = user.username
+         var Sid = this.$route.query.id
+         this.$axios({
+           method: 'get',
+           url: '/api/survey/write/' + Sid,
+           }).then((res) => {   
+          this.title = res.data.title
+          var len = res.data.choice.length
+          for (var i = 0; i < len; i++) {
+            res.data.choice[i].radio=1
+             }
+          this.questionData.choice=res.data.choice    
+         var tmpJson=res.data.questionAnswer
+   
+         var len2=tmpJson.length
+         for (var i=0; i < len2; i++) { 
+               tmpJson.answer=' '
+                }
+           this.questionData.questionAnswer=tmpJson 
+           this.questionAnswer.questionAnswer=res.data.questionAnswer 
+            }
+           ).catch((error)=> {
+        
+           })
 
-      console.log(Sid)
-      this.$axios({
-                       method: 'get',
-                       url: 'http://106.15.206.229/survey/write/'+Sid,
-                           
-                            
-                        }).then((res) => {
-                            console.log('111');
-                            console.log(res.data.choice);
-                            this.title=res.data.title
-                            this.questionData.choice=res.data.choice
-                            var len=this.questionData.choice.length
-                            for(var i =0 ; i < len ; i++){
-                              this.questionData.choice[i].radio="1"
-                            }
-                            var tmpJson=res.data.questionAnswer
-                            console.log(tmpJson)
-                            var len2=tmpJson.length
-                            for(var i = 0 ; i < len2 ; i++ ){
-                              tmpJson.answer=' '
-                            }
-                            this.questionData.questionAnswer=tmpJson
-                            //this.questionAnswer.questionAnswer=res.data.questionAnswer
-                            // console.log(this.title)
-                            // 
-                             var user=JSON.parse(sessionStorage.getItem('user'));
-                             console.log(user.username)
-                             this.username=uer.username
-                             console.log('hahhah')
-                            console.log(this.questionData)
-                        }).catch((error) => {
-                             
-                            console.log('调查获取失败');
-                            console.log(error.response.data);
-                            console.log(error.response.status);
-                            console.log(error.response.headers); 
-                            console.log('Error', error.message);
-                            console.log(error.config);
-                        })
+           
+           
+    },
+    mounted() {
+    }
+  };
 
-    // this.handleCurrentChange(1);
-  }
-};
 </script>
 
 <style scoped>
-.locat {
-  position: absolute;
-  top: 20px;
-  left: 40px;
-}
-.content {
-  /* border: 1px solid #000; */
-  position: absolute;
-  width: 100%;
-  top: 70px;
-}
-.content h3 {
-  margin-left: 40px;
-}
-#table {
-  width: 80%;
-  height: 100%;
-  margin: 0 auto;
-}
-#page {
-  width: 80%;
-  margin: 0 auto;
-  padding: 30px;
-  text-align: center;
-}
-#add {
-  margin-left: 82%;
-  margin-bottom: 10px;
-}
-#choice{
-  text-align: center;
-}
-#questionAnswer{
-  text-align: center;
-}
-.eInput{
-  width:350px;
-  border-radius: 4px;
-  border:1px solid #dcdfe6;
-  height: 40px;
-  line-height:40px;
-      -webkit-appearance: none;
+  .locat {
+    position: absolute;
+    top: 20px;
+    left: 40px;
+  }
+
+  .content {
+    /* border: 1px solid #000; */
+    position: absolute;
+    width: 100%;
+    top: 70px;
+  }
+
+  .content h3 {
+    margin-left: 40px;
+  }
+
+  #table {
+    width: 80%;
+    height: 100%;
+    margin: 0 auto;
+  }
+
+  #page {
+    width: 80%;
+    margin: 0 auto;
+    padding: 30px;
+    text-align: center;
+  }
+
+  #add {
+    margin-left: 82%;
+    margin-bottom: 10px;
+  }
+
+  #choice {
+    text-align: center;
+  }
+
+  #questionAnswer {
+    text-align: center;
+  }
+
+  .eInput {
+    width: 350px;
+    border-radius: 4px;
+    border: 1px solid #dcdfe6;
+    height: 40px;
+    line-height: 40px;
+    -webkit-appearance: none;
     background-color: #fff;
     background-image: none;
     border-radius: 4px;
@@ -320,11 +241,13 @@ export default {
     line-height: 40px;
     outline: none;
     padding: 0 15px;
-    transition: border-color .2s cubic-bezier(.645,.045,.355,1);
-    
-}
-#submit{
-  margin-top:50px; 
-  text-align: center;
-}
+    transition: border-color .2s cubic-bezier(.645, .045, .355, 1);
+
+  }
+
+  #submit {
+    margin-top: 50px;
+    text-align: center;
+  }
+
 </style>
